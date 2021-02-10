7ded5fc0a9d242dee7fea31ed2d600387e838af7
João Carlos Clementoni <joaofx@gmail.com>
2021-02-10 19:21:10 +0100

    Refactoring MiruCli

---
+++ src/MiruCli/AtAppCommand.cs
@@ -0,0 +1,37 @@
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+using Miru.Core;
+using MiruCli.Process;
+
+namespace MiruCli
+{
+    public class AtAppCommand : Command
+    {
+        private readonly MiruSolution _miruSolution;
+
+        public AtAppCommand(string commandName, MiruSolution miruSolution) : base(commandName)
+        {
+            _miruSolution = miruSolution;
+            
+            AddArgument(new Argument<string>("executable") { Arity = ArgumentArity.ExactlyOne });
+            AddArgument(new Argument<string[]>("args") { Arity = ArgumentArity.ZeroOrMore });
+            
+            Handler = CommandHandler.Create((string executable, string[] args) => Execute(executable, args));
+        }
+
+        private void Execute(string executable, string[] args)
+        {
+            var processRunner = new MiruProcessRunner(prefix: string.Empty);
+
+            var proc = processRunner.RunAsync(new ProcessSpec()
+            {
+                Executable = executable,
+                Arguments = args,
+                WorkingDirectory = _miruSolution.AppDir
+            });
+
+            Task.WaitAll(proc);
+        }
+    }
+}
--- src/MiruCli/Program.cs
+++ src/MiruCli/Program.cs
@@ -1,6 +1,11 @@
-﻿using System.CommandLine;
+﻿using System.Collections.Generic;
+using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.IO;
 using System.Threading.Tasks;
+using Baseline;
+using Miru.Core;
+using MiruCli.Process;
 
 namespace MiruCli
 {
@@ -8,19 +13,60 @@ namespace MiruCli
     {
         public static async Task Main(string[] args)
         {
-            var rootCommand = new RootCommand
-            {
-                new ShowProjectCommand("app", m => m.AppDir),
-                new ShowProjectCommand("tests", m => m.AppTestsDir),
-                new ShowProjectCommand("pagetests", m => m.AppPageTestsDir),
-                new SetupCommand("setup"),
-                new NewCommand("new"),
-                new WatchCommand("watch", m => m.AppDir),
-            };
+            var result = new SolutionFinder().FromDir(@"D:\Intan\Temp\Intanext.Shopworld\src\Intanext.Shopworld");
 
-            rootCommand.Name = "miru";
+            if (result.FoundSolution == false)
+            {
+                var rootCommand = new RootCommand
+                {
+                    // new ShowProjectCommand("app", m => m.AppDir),
+                    // new ShowProjectCommand("tests", m => m.AppTestsDir),
+                    // new ShowProjectCommand("pagetests", m => m.AppPageTestsDir),
+                    // new SetupCommand("setup"),
+                    
+                    new NewCommand("new"),
+                };
+
+                rootCommand.Name = "miru";
             
-            await rootCommand.InvokeAsync(args);
+                await rootCommand.InvokeAsync(args);
+            }
+            else
+            {
+                var solution = result.Solution;
+                
+                var rootCommand = new RootCommand
+                {
+                    new AtAppCommand("app", solution),
+                    new AtTestCommand("test", solution),
+                    new AtPageTestCommand("pagetest", solution),
+                    new WatchCommand("watch", m => m.AppDir),
+                };
+                
+                rootCommand.AddArgument(new Argument<string[]>("args") { Arity = ArgumentArity.ZeroOrMore });
+                rootCommand.Handler = CommandHandler.Create((string[] miruArgs) => Execute(solution, miruArgs));
+                
+                await rootCommand.InvokeAsync(args);
+            }
+        }
+
+        public static void Execute(MiruSolution solution, string[] args)
+        {
+            var processRunner = new MiruProcessRunner(prefix: string.Empty);
+            var mergedArgs = new List<string>();
+            mergedArgs.AddRange(new[] {"run", "miru"});
+            
+            if (args?.Length > 0)
+                mergedArgs.AddRange(args);
+
+            var proc = processRunner.RunAsync(new ProcessSpec()
+            {
+                Executable = "dotnet",
+                Arguments = mergedArgs,
+                WorkingDirectory = solution.AppDir
+            });
+
+            Task.WaitAll(proc);
         }
     }
 }
