fb2c2b0c5246fda24531a5dec1b32444f82a8ad5
Michel <michel.anjos@gmail.com>
2021-04-08 21:12:45 +0100

    Adding the possibility to run jobs recurrently

--- src/Miru/Queuing/Jobs.cs
+++ src/Miru/Queuing/Jobs.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using Hangfire;
 
@@ -6,15 +7,25 @@ namespace Miru.Queuing
     public class Jobs
     {
         private readonly IBackgroundJobClient _backgroundJobClient;
+        private readonly IRecurringJobManager _recurringJobManager;
 
-        public Jobs(IBackgroundJobClient backgroundJobClient)
+        public Jobs(IBackgroundJobClient backgroundJobClient, IRecurringJobManager recurringJobManager)
         {
             _backgroundJobClient = backgroundJobClient;
+            _recurringJobManager = recurringJobManager;
         }
 
         public void PerformLater<TJob>(TJob job) where TJob : IJob
         {
             _backgroundJobClient.Enqueue<JobFor<TJob>>(m => m.Execute(job, CancellationToken.None));
+        }
+
+        public string PerformLater<TJob>(TJob job, string cronExpression) where TJob : IJob
+        {
+            var jobId = Guid.NewGuid().ToString();
+            _recurringJobManager.AddOrUpdate<JobFor<TJob>>(jobId,
+                m => m.Execute(job, CancellationToken.None), cronExpression);
+            return jobId;
         }
     }
 }
