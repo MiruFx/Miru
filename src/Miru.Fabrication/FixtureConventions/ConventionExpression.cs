7bfce2c5906488ade170a45afa6bd8a9581c5fb7
João Carlos Clementoni <joaofx@gmail.com>
2021-10-19 01:11:47 +0200

    converting to .net 6

--- src/Miru.Fabrication/FixtureConventions/ConventionExpression.cs
+++ src/Miru.Fabrication/FixtureConventions/ConventionExpression.cs
@@ -41,6 +41,16 @@ namespace Miru.Fabrication.FixtureConventions
             return new UseExpression(() => useFaker(_faker), this);
         }
         
+        public UseExpression Use<TProperty>(Func<Faker, TProperty> useFaker)
+        {
+            return new UseExpression(() => useFaker(_faker), this);
+        }
+        
+        public UseExpression Use<TProperty>(TProperty useValue)
+        {
+            return new UseExpression(() => useValue, this);
+        }
+        
         public UseExpression Use(object useValue)
         {
             return new UseExpression(() => useValue, this);
