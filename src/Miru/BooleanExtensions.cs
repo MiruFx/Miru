23725c38cdfa8d79515fbf6fa422dd0205e7b579
João Carlos Clementoni <joaofx@gmail.com>
2022-03-03 00:03:49 +0100

    updated dependencies

--- src/Miru/BooleanExtensions.cs
+++ src/Miru/BooleanExtensions.cs
@@ -2,15 +2,6 @@ namespace Miru;
 
 public static class BooleanExtensions
 {
-    private const string True = "true";
-    private const string False = "false";
-    
-    public static string If(this string contentToShow, bool condition) =>
-        condition ? contentToShow : string.Empty;
-    
-    public static string IfNot(this string contentToShow, bool condition) =>
-        condition == false ? contentToShow : string.Empty;
-    
     public static TReturn IfTrueThen<TReturn>(this bool value, TReturn @return)
     {
         return value ? @return : default;
