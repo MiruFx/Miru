1c908092f9c6d0a4c4b4a34079e0fe35267ddc1b
João Carlos Clementoni <joaofx@gmail.com>
2022-12-14 19:46:02 +0100

    Support arguments for invocables

--- src/Miru/Pipeline/InvokeConsolable.cs
+++ src/Miru/Pipeline/InvokeConsolable.cs
@@ -1,10 +1,15 @@
 using System;
 using System.CommandLine;
+using System.CommandLine.Binding;
+using System.CommandLine.Invocation;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using MediatR;
 using Miru.Consolables;
 using Miru.Core;
+using Miru.Foundation.Bootstrap;
 
 namespace Miru.Pipeline;
 
@@ -20,10 +25,12 @@ public class InvokeConsolable : Consolable
         public string Name { get; set; }
 
         private readonly IMiruApp _app;
+        private readonly ArgsConfiguration _argsConfig;
 
-        public ConsolableHandler(IMiruApp app)
+        public ConsolableHandler(IMiruApp app, ArgsConfiguration argsConfig)
         {
             _app = app;
+            _argsConfig = argsConfig;
         }
                 
         public async Task Execute()
@@ -43,6 +50,23 @@ public class InvokeConsolable : Consolable
 
             var instance = Activator.CreateInstance(type);
 
+            if (instance is RootCommand rootCommand)
+            {
+                rootCommand.TreatUnmatchedTokensAsErrors = false;
+                
+                var binder = new ModelBinder(instance.GetType())
+                {
+                    EnforceExplicitBinding = false
+                };
+                
+                // excludes from args 'invoke FeatureName'
+                var result = rootCommand.Parse(_argsConfig.CliArgs[2..]);
+                var invocationContext = new InvocationContext(result);
+                var bindingContext = invocationContext.BindingContext;
+
+                binder.UpdateInstance(instance, bindingContext);
+            }
+            
             if (instance is IInvokable request)
             {
                 await _app.ScopedSendAsync(request);
---
+++ tests/Miru.Tests/Pipelines/InvokeConsolableTest.cs
@@ -0,0 +1,123 @@
+using System;
+using System.CommandLine;
+using System.IO;
+using System.Threading;
+using MediatR;
+using Miru.Config;
+using Miru.Hosting;
+using Miru.Pipeline;
+using StringWriter = System.IO.StringWriter;
+
+namespace Miru.Tests.Pipelines;
+
+public class InvokableConsolableTest
+{
+    private TextWriter _defaultConsoleWriter;
+    private StringWriter _outWriter;
+    private TextWriter _defaultConsoleError;
+    private StringWriter _errorWriter;
+
+    [SetUp]
+    public void Setup()
+    {
+        _defaultConsoleWriter = Console.Out;
+        _defaultConsoleError = Console.Error;
+            
+        _outWriter = new StringWriter();
+        _errorWriter = new StringWriter();
+            
+        Console.SetOut(_outWriter);
+        Console.SetError(_errorWriter);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Console.SetOut(_defaultConsoleWriter);
+        Console.SetError(_defaultConsoleError);
+    }
+
+    [Test]
+    public async Task Should_run_invokable_without_args()
+    {
+        // arrange
+        var host = MiruHost.CreateMiruHost("miru", "invoke", "OrderExport").ConfigureServices(x => x
+            .AddMiruApp<InvokableConsolableTest>()
+            .AddConsolables<ConfigShowConsolable>()
+            .AddPipeline<InvokableConsolableTest>());
+                
+        // act
+        await host.RunMiruAsync();
+            
+        // assert
+        var output = _outWriter.ToString();
+            
+        Console.WriteLine(output);
+
+        output.ShouldContain("Exporting");
+    }
+
+    [Test]
+    public async Task Should_run_invokable_parsing_args_to_create_command_instance()
+    {
+        // arrange
+        var host = MiruHost.CreateMiruHost("miru", "invoke", "OrderArchive", "--order-id", "123", "--archive-type", "Temporarily").ConfigureServices(x => x
+            .AddMiruApp<InvokableConsolableTest>()
+            .AddConsolables<ConfigShowConsolable>()
+            .AddPipeline<InvokableConsolableTest>());
+                
+        // act
+        await host.RunMiruAsync();
+            
+        // assert
+        var output = _outWriter.ToString();
+            
+        output.ShouldContain("123 Temporarily");
+    }
+
+    public class OrderArchive
+    {
+        public class Command : RootCommand, IRequest<Command>, IInvokable
+        {
+            public Command()
+            {
+                Add(new Option<long>("--order-id"));
+                Add(new Option<ArchiveType>("--archive-type"));
+            }
+
+            public long OrderId { get; set; }
+            public ArchiveType ArchiveType { get; set; }
+        }
+            
+        public class Handler : IRequestHandler<Command, Command>
+        {
+            public async Task<Command> Handle(Command request, CancellationToken cancellationToken)
+            {
+                Console.WriteLine($"{request.OrderId} {request.ArchiveType}");
+                return await Task.FromResult(request);
+            }
+        }
+    }
+
+    public enum ArchiveType
+    {
+        Permanent,
+        Temporarily
+    }
+    
+    public class OrderExport
+    {
+        public class Command : IRequest<Command>, IInvokable
+        {
+        }
+            
+        public class Handler : IRequestHandler<Command, Command>
+        {
+            public async Task<Command> Handle(Command request, CancellationToken cancellationToken)
+            {
+                Console.WriteLine($"Exporting");
+                return await Task.FromResult(request);
+            }
+        }
+    }
+}
